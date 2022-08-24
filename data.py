fa4937f7cbad30ab736f2d1522673ae7d6b2e4af
Martin Fiedler <keyj@emphy.de>
2022-08-24 23:15:28 +0200

    first version with complete(?) content and functionality

--- data.py
+++ data.py
@@ -1,5 +1,7 @@
 def wrappers(m):
     m.title = "Graphics API Wrapper Matrix"
+    m.coltitle = "Source API"
+    m.rowtitle = "Target API"
 
     d3d7  = m.add_category("Direct3D 7/8")
     d3d9  = m.add_category("Direct3D 9")
@@ -11,6 +13,22 @@ def wrappers(m):
     vk    = m.add_category("Vulkan")
     mtl   = m.add_category("Metal")
 
-    m.add_item((d3d9, d3d11), vk,    "DXVK",     "https://github.com/doitsujin/dxvk")
-    m.add_item( d3d9,         d3d11, "DXUP",     "https://github.com/Joshua-Ashton/dxup")
-    m.add_item( d3d9,         d3d12, "D3D9on12", "https://github.com/microsoft/D3D9On12")
+    in_mesa = m.add_footnote('implemented as part of the <a href="https://www.mesa3d.org" target="_blank">Mesa</a> project')
+
+    # specific wrappers
+    m.add_item((d3d9, d3d11),       vk,             "DXVK",         "https://github.com/doitsujin/dxvk")
+    m.add_item( d3d12,              vk,             "VKD3D",        "https://wiki.winehq.org/Vkd3d")
+    m.add_item( d3d12,              vk,             "VKD3D-Proton", "https://github.com/HansKristian-Work/vkd3d-proton")
+    m.add_item( d3d9,               d3d11,          "DXUP",         "https://github.com/Joshua-Ashton/dxup")
+    m.add_item( d3d9,               d3d12,          "D3D9On12",     "https://github.com/microsoft/D3D9On12")
+    m.add_item( d3d11,              d3d12,          "D3D11On12",    "https://github.com/microsoft/D3D11On12")
+    m.add_item( vk,                 mtl,            "MoltenVK",     "https://github.com/KhronosGroup/MoltenVK")
+    m.add_item( gl,                 vk,             "Zink",         "https://docs.mesa3d.org/drivers/zink.html")
+    m.add_item( gl,                 gles2,          "GL4ES",        "http://ptitseb.github.io/gl4es/")
+    m.add_item( gl,                 d3d12,          "OpenGLOn12",   "https://docs.mesa3d.org/drivers/d3d12.html", in_mesa)
+    m.add_item( vk,                 d3d12,          "Dozen",        "https://gitlab.freedesktop.org/mesa/mesa/-/tree/main/src/microsoft/vulkan", in_mesa)
+
+    # universal wrappers
+    m.add_item( gles2, (gl, d3d9, d3d11, gles2, vk, mtl),  "ANGLE", "https://chromium.googlesource.com/angle/angle/+/main/README.md")
+    m.add_item((d3d7, d3d9, d3d11), (gl, vk),       "WineD3D",      "https://www.winehq.org")
+    m.add_item((d3d7, d3d9),        (d3d11, d3d12), "dgVoodoo 2",   "http://dege.freeweb.hu/dgVoodoo2/")
--- generate.py
+++ generate.py
@@ -13,9 +13,9 @@ class MatrixCategory:
         self.order = order
 
 class MatrixFootnote:
-    def __init__(self, html, number=0):
+    def __init__(self, html):
         self.html = html
-        self.number = number
+        self.number = 0
 
 class MatrixItem:
     def __init__(self, cols, rows, name, url, notes):
@@ -27,11 +27,14 @@ class MatrixItem:
         self.notes = notes
 
 class Matrix:
-    def __init__(self):
+    def __init__(self, obj_id=None):
+        self.obj_id = obj_id
         self.categories = []
-        self.footnotes = []
+        self.notes = []
         self.items = []
         self.title = None
+        self.coltitle = None
+        self.rowtitle = None
 
     def add_category(self, html):
         cat = MatrixCategory(html, len(self.categories))
@@ -39,15 +42,15 @@ class Matrix:
         return cat
 
     def add_footnote(self, html):
-        note = MatrixFootnote(html, len(self.footnotes) + 1)
-        self.footnotes.append(note)
+        note = MatrixFootnote(html)
+        self.notes.append(note)
         return note
 
     def add_item(self, cols, rows, name, *url_and_footnotes):
         url = [obj for obj in url_and_footnotes if not isinstance(obj, MatrixFootnote)]
         if len(url) > 1: raise ValueError("multiple URLs specified for a matrix item")
-        url = url.pop()
-        if not isinstance(url, str): raise TypeError("URL must be a string")
+        if url: url = url.pop()
+        if url and not(isinstance(url, str)): raise TypeError("URL must be a string")
         notes = [obj for obj in url_and_footnotes if isinstance(obj, MatrixFootnote)]
         item = MatrixItem(cols, rows, name, url, notes)
         self.items.append(item)
@@ -63,33 +66,57 @@ class Matrix:
 
         # produce header
         if self.title: yield f'<h2>{self.title}</h2>'
-        yield '<table class="matrix"><tr><td></td>'
+        spandecl = ""
+        if self.coltitle: spandecl += ' rowspan="2"'
+        if self.rowtitle: spandecl += ' colspan="2"'
+        yield f'<table id="{self.obj_id}" class="matrix"><tr><td class="nocell"{spandecl}></td>'
+        if self.coltitle:
+            yield f'<th class="coltitle" colspan="{len(cols)}">{self.coltitle}</th>'
+            yield f'</tr><tr>'
         for i, col in enumerate(self.categories):
             if i in cols:
                 yield f'<th>{col.html}</th>'
         yield '</tr>'
 
-        # produce cells
+        # produce cells (and assign footnote indexes as we go)
+        note_count = 0
+        first_row = True
         for j, row in enumerate(self.categories):
             if not(j in rows): continue
-            yield f'<tr><th>{row.html}</th>'
+            yield f'<tr>'
+            if first_row and self.rowtitle:
+                yield f'<th class="rowtitle" rowspan="{len(rows)}"><div>{self.rowtitle}</div></th>'
+            yield f'<th>{row.html}</th>'
             for i, col in enumerate(self.categories):
                 if not(i in cols): continue
-                first = True
+                first_line = True
                 prefix = '<td>'
                 for item in self.items:
                     if not((i,j) in item.cells): continue
-                    line = '<td>' if first else '<br>'
+                    line = ('<td>' if first_line else '') + '<div>'
                     if item.url: line += f'<a href="{item.url}" target="_blank">'
                     line += item.name
                     if item.url: line += f'</a>'
-                    yield line
-                    first = False
-                yield ('<td class="empty">&mdash;</td>' if first else '</td>')
+                    for note in item.notes:
+                        if not note.number:
+                            note_count += 1
+                            note.number = note_count
+                        line += f'<sup><a href="#{self.obj_id}-note-{note.number}">{note.number}</a></sup>'
+                    yield line + '</div>'
+                    first_line = False
+                yield ('<td class="empty">&mdash;</td>' if first_line else '</td>')
             yield '</tr>'
+            first_row = False
 
         # produce footer
         yield '</table>'
+        if any(note.number for note in self.notes):
+            yield '<ol id="{self.obj_id}-notes" class="footnotes">'
+            for note in sorted(self.notes, key=lambda n: n.number):
+                if not note.number: continue
+                yield f'<li id="{self.obj_id}-note-{note.number}" value="{note.number}">{note.html}</li>'
+            yield '</ol>'
+        # TODO: handle footnotes (not yet done, because there aren't any)
 
 ################################################################################
 
@@ -100,8 +127,9 @@ if __name__ == "__main__":
         html = f.read()
 
     def replace_object(m):
-        obj = globals()[m.group(1).capitalize()]()
-        getattr(data, m.group(2))(obj)
+        obj_id = m.group(2)
+        obj = globals()[m.group(1).capitalize()](obj_id)
+        getattr(data, obj_id)(obj)
         return '\n'.join(obj.to_html())
     html = re.sub(r'<(matrix) id="([^"]+)">', replace_object, html)
 
