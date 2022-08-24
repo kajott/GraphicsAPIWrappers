63732ccd3523ff426755b68f7c87115222ba7e00
Martin Fiedler <keyj@emphy.de>
2022-08-24 17:26:35 +0200

    established data model, made first entries

--- data.py
+++ data.py
@@ -1,2 +1,16 @@
 def wrappers(m):
-    pass
+    m.title = "Graphics API Wrapper Matrix"
+
+    d3d7  = m.add_category("Direct3D 7/8")
+    d3d9  = m.add_category("Direct3D 9")
+    d3d11 = m.add_category("Direct3D 11")
+    d3d12 = m.add_category("Direct3D 12")
+    gl    = m.add_category("Desktop OpenGL")
+    gles1 = m.add_category("OpenGL ES 1.x")
+    gles2 = m.add_category("OpenGL ES 2.x/3.x")
+    vk    = m.add_category("Vulkan")
+    mtl   = m.add_category("Metal")
+
+    m.add_item((d3d9, d3d11), vk,    "DXVK",     "https://github.com/doitsujin/dxvk")
+    m.add_item( d3d9,         d3d11, "DXUP",     "https://github.com/Joshua-Ashton/dxup")
+    m.add_item( d3d9,         d3d12, "D3D9on12", "https://github.com/microsoft/D3D9On12")
--- generate.py
+++ generate.py
@@ -7,9 +7,89 @@ import data
 
 ################################################################################
 
+class MatrixCategory:
+    def __init__(self, html, order=0):
+        self.html = html
+        self.order = order
+
+class MatrixFootnote:
+    def __init__(self, html, number=0):
+        self.html = html
+        self.number = number
+
+class MatrixItem:
+    def __init__(self, cols, rows, name, url, notes):
+        if isinstance(cols, MatrixCategory): cols = [cols]
+        if isinstance(rows, MatrixCategory): rows = [rows]
+        self.cells = { (col.order, row.order) for col in cols for row in rows }
+        self.name  = name
+        self.url   = url
+        self.notes = notes
+
 class Matrix:
-    def html(self):
-        pass
+    def __init__(self):
+        self.categories = []
+        self.footnotes = []
+        self.items = []
+        self.title = None
+
+    def add_category(self, html):
+        cat = MatrixCategory(html, len(self.categories))
+        self.categories.append(cat)
+        return cat
+
+    def add_footnote(self, html):
+        note = MatrixFootnote(html, len(self.footnotes) + 1)
+        self.footnotes.append(note)
+        return note
+
+    def add_item(self, cols, rows, name, *url_and_footnotes):
+        url = [obj for obj in url_and_footnotes if not isinstance(obj, MatrixFootnote)]
+        if len(url) > 1: raise ValueError("multiple URLs specified for a matrix item")
+        url = url.pop()
+        if not isinstance(url, str): raise TypeError("URL must be a string")
+        notes = [obj for obj in url_and_footnotes if isinstance(obj, MatrixFootnote)]
+        item = MatrixItem(cols, rows, name, url, notes)
+        self.items.append(item)
+        return item
+
+    def to_html(self):
+        # survey all used cells
+        cells = set()
+        for item in self.items:
+            cells |= item.cells
+        cols = { col for (col, row) in cells }
+        rows = { row for (col, row) in cells }
+
+        # produce header
+        if self.title: yield f'<h2>{self.title}</h2>'
+        yield '<table class="matrix"><tr><td></td>'
+        for i, col in enumerate(self.categories):
+            if i in cols:
+                yield f'<th>{col.html}</th>'
+        yield '</tr>'
+
+        # produce cells
+        for j, row in enumerate(self.categories):
+            if not(j in rows): continue
+            yield f'<tr><th>{row.html}</th>'
+            for i, col in enumerate(self.categories):
+                if not(i in cols): continue
+                first = True
+                prefix = '<td>'
+                for item in self.items:
+                    if not((i,j) in item.cells): continue
+                    line = '<td>' if first else '<br>'
+                    if item.url: line += f'<a href="{item.url}" target="_blank">'
+                    line += item.name
+                    if item.url: line += f'</a>'
+                    yield line
+                    first = False
+                yield ('<td class="empty">&mdash;</td>' if first else '</td>')
+            yield '</tr>'
+
+        # produce footer
+        yield '</table>'
 
 ################################################################################
 
@@ -22,7 +102,7 @@ if __name__ == "__main__":
     def replace_object(m):
         obj = globals()[m.group(1).capitalize()]()
         getattr(data, m.group(2))(obj)
-        return obj.html()
+        return '\n'.join(obj.to_html())
     html = re.sub(r'<(matrix) id="([^"]+)">', replace_object, html)
 
     with open("index.html", 'w', encoding='utf-8') as f:
